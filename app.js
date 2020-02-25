ddd20f0cf039041e7de8942a960dbf7b3ac8e48d
ghaedi1993 <ghaedi1993@gmail.com>
2020-02-25 20:53:43 +0330

    setting baseground for the project ,adding mongodb connection plus app listenting to a port definded in config file

---
+++ app.js
@@ -0,0 +1,17 @@
+const express       = require('express'), 
+      bodyParser    = require    ('body-parser'),
+      mongoose      = require    ('mongoose'),
+      keys          = require    ('./config/keys')
+
+app = express()
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json())
+
+const mongoConnection = mongoose.connect(keys.mongoUrl,{useUnifiedTopology:true,useNewUrlParser:true})
+.then(()=>console.log('connected to MongoDB SuccessFully'))
+.catch((err)=>console.log(err))
+
+const server = app.listen(keys.port,(req, res)=>{
+    console.log('app is running and listening to port',keys.port)
+})
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoUrl : 'mongodb+srv://aranic:aranic@cluster0-1fc24.mongodb.net/MasterDb?retryWrites=true&w=majority',
+    port       : '8080'
+}
