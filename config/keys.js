207c045712a92c802b41b0ab784a2e2d029ba989
ghaedi1993 <ghaedi1993@gmail.com>
2020-02-27 00:38:51 +0330

    changed config file

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
     // mongoUrl : 'mongodb+srv://aranic:aranic@cluster0-1fc24.mongodb.net/MasterDb?retryWrites=true&w=majority',
-    exports.mongoUrl = 'mongodb://localhost:27017/sakok',
-    exports.PORT      = '8080'
+    const mongoUrl = 'mongodb://localhost:27017/sakok',
+          PORT      = '8080'
+
+    module.exports = {PORT, mongoUrl};
